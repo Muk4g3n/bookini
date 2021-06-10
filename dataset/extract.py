2b51b71763dbb817783e0895b1803cc795655b97
KESSIR Ouassim <wassim.kes213@gmail.com>
2021-06-10 14:19:44 +0100

    update

---
+++ dataset/extract.py
@@ -0,0 +1,6 @@
+import pickle
+import pandas as pd
+import math
+data = pickle.load(open('SoftCosineSimilarity_final_WES2.pickle','rb'))
+print(data)
+
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask,render_template,url_for,request,redirect, session
+from flask import Flask,render_template,url_for,request,redirect, session , jsonify
 from flask_pymongo import PyMongo
+import pandas as pd 
+import numpy as np
+import pickle
+from bson.json_util import dumps
 
 app = Flask(__name__)
 app.config['MONGO_URI'] ='mongodb://localhost:27017/dataset'
@@ -15,11 +19,26 @@ def dashboard():
         return render_template('dashbord.html',books = books)
     return redirect('/login')
 
-@app.route('/<int:id>')
+@app.route('/<int:id>',methods = ['GET','POST'])
 def book(id):
     if 'u_id' in session:
-        result = mongo.db.books.find_one_or_404({"ISBN":id})
-        return render_template('book.html',res = result)
+        # if request.method == 'POST':
+        #     newrating = {
+        #         'User-ID' : session['u_id'],
+        #         'ISBN' : id,
+        #         'Book-Rating' : int(request.form['rating'])
+        #     }
+            
+        #     # mongo.db.ratings.insert_one(newrating)
+        #     current_book = mongo.db.books.find_one({'ISBN':id}) 
+        #     average = ((current_book['SommeRating'] * current_book['average_rating']) + newrating['Book-Rating']) / (current_book['SommeRating'] + 1)
+        #     current_book['average_rating'] = average              
+        #     current_book['SommeRating'] = current_book['SommeRating'] + 1
+        #     mongo.db.books.update_one({'ISBN' : id}, {"$set" : current_book})
+        #     return redirect('/')
+        # else:
+            result = mongo.db.books.find_one_or_404({"ISBN":id})
+            return render_template('book.html',res = result)
     return redirect('/login')
 
 @app.route('/login',methods= ['POST','GET'])
@@ -79,11 +98,17 @@ def signup():
 def cats():
     if request.method == 'POST':
         cat =  request.form.getlist('cat')
-        print(cat)
         return redirect('/')
     else:
         categories = mongo.db.categories.find({}).sort([('nb',-1)])
         return render_template('categories.html',categories = categories)
+
+
+@app.route('/similair')
+def sim():
+    res = mongo.db.books.find()
+    list_res = list(res)[:5]
+    return dumps(list_res)
 
 
 @app.route('/logout')
